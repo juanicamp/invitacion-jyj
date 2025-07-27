3673cf34155ee4b2144d2979ca46239c09289f28
Campora Juan Ignacio <104077946+juanicamp@users.noreply.github.com>
2025-07-27 12:41:16 -0300

    ok4

---
+++ script.js
@@ -0,0 +1,5 @@
+
+document.getElementById('openButton').addEventListener('click', function () {
+  document.getElementById('cover').style.display = 'none';
+  document.getElementById('gallery').classList.remove('hidden');
+});
